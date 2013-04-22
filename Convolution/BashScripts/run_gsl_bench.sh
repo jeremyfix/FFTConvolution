54ce262670d2cf60f5f5b3dcde78db54f3df2de8
jeremyfix <jeremy.fix@gmail.com>
2013-04-22 14:55:34 +0200

    adding benchmark scripts for gsl and fftw

---
+++ Convolution/BashScripts/run_gsl_bench.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+rm ../Data/benchmarks_convolution_gsl.txt
+
+MAX_SIZE=20
+
+cd ../bin
+echo "Running convolution_gsl_benchmark_linear.bin"
+for i in $(seq 3 $MAX_SIZE)
+do
+    for j in $(seq 3 $(($i-1)))
+    do
+	./convolution_gsl_benchmark_linear.bin $i $j > /dev/null
+    done
+done
+echo "Running convolution_gsl_benchmark_linear_optimal.bin"
+for i in $(seq 3 $MAX_SIZE)
+do
+    for j in $(seq 3 $(($i-1)))
+    do
+	./convolution_gsl_benchmark_linear_optimal.bin $i $j > /dev/null
+    done
+done
+
+
+echo "Running convolution_gsl_benchmark_circular.bin"
+for i in $(seq 3 $MAX_SIZE)
+do
+    for j in $(seq 3 $(($i-1)))
+    do
+	./convolution_gsl_benchmark_circular.bin $i $j > /dev/null
+    done
+done
+echo "Running convolution_gsl_benchmark_circular_optimal.bin"
+for i in $(seq 3 $MAX_SIZE)
+do
+    for j in $(seq 3 $(($i-1)))
+    do
+	./convolution_gsl_benchmark_circular_optimal.bin $i $j > /dev/null
+    done
+done
+
+cd ../BashScripts
+
+
